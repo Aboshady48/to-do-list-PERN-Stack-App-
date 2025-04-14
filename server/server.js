814e38b6c6f031c252d0a3d04d6f15142b14dcf4
Aboshady48 <aboshady48585@gmail.com>
2025-04-14 13:23:56 +0200

    creating the server side folder

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const cors = require('cors')
+const PORT = 3000;
+
+//Middelware
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
